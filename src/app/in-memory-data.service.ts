71a51dd81cf87f52c46ab39c9ad62fd78f880782
ivan <ivantanasi111@gmail.com>
2017-09-08 18:06:01 +0200

    experience page, more details for description

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,15 +3,16 @@ export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const experiences = [
             {
-                id: 111, name: 'Angular', description: 'Frontend JavaScript Framework', type: 'JavaScript Framework',
-                iconPath: './assets/img/ico/angular.ico'
+                id: 111, name: 'Angular', description: 'Angular is a platform that makes it easy to build applications with the web',
+                type: 'JavaScript Framework',
+                iconPath: './assets/img/ico/angular.png'
             },
             {
-                id: 112, name: 'React', description: 'Frontend JavaScript Framework', type: 'JavaScript Framework',
+                id: 112, name: 'React', description: 'JavaScript library for building user interfaces', type: 'JavaScript Framework',
                 iconPath: './assets/img/ico/react.png'
             },
             {
-                id: 113, name: 'Vue.js', description: 'Frontend JavaScript Framework', type: 'JavaScript Framework',
+                id: 113, name: 'Vue.js', description: 'Vue is a progressive framework for building user interfaces', type: 'JavaScript Framework',
                 iconPath: './assets/img/ico/vuejs.png'
             },
             {
@@ -32,42 +33,47 @@ export class InMemoryDataService implements InMemoryDbService {
             {
                 id: 116,
                 name: 'Notepad++',
-                description: `Notepad++`,
+                description: `Notepad++ is a free source code editor and Notepad replacement that supports several languages`,
                 type: 'Code editor',
                 iconPath: './assets/img/ico/notepad++.jpg'
             },
             {
                 id: 117,
                 name: 'Visual Studio',
-                description: `Visual Studio`,
+                description: `Fully-featured integrated development environment (IDE)
+                              for Android, iOS, Windows, web, and cloud`,
                 type: 'IDE',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 118,
-                name: 'TFS',
-                description: `TFS`,
+                name: 'Team Foundation Server',
+                description: `Team Foundation Server (commonly abbreviated to TFS) is a Microsoft product that provides source code management 
+                              (either with Team Foundation Version Control or Git)`,
                 type: 'Source control',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 119,
                 name: 'Git',
-                description: `Git`,
+                description: `Git is a free and open source distributed version control system designed to handle everything from small to 
+                              very large projects with speed and efficiency`,
                 type: 'Source control',
                 iconPath: './assets/img/ico/git.ico'
             },
             {
                 id: 120,
                 name: 'Windows',
-                description: `Windows`,
+                description: `Microsoft Windows, or simply Windows, is a metafamily of graphical operating systems developed, marketed, 
+                              and sold by Microsoft`,
                 type: 'OS',
                 iconPath: './assets/img/ico/windows.png'
             },
             {
                 id: 121,
                 name: 'Linux',
-                description: `Linux`,
+                description: `Linux is a Unix-like computer operating system assembled under the model of free and open-source software 
+                              development and distribution`,
                 type: 'OS',
                 iconPath: './assets/img/ico/linux.png'
             },
@@ -75,57 +81,62 @@ export class InMemoryDataService implements InMemoryDbService {
                 id: 122,
                 name: 'ASP.NET Web Forms',
                 description: `ASP.NET Web Forms`,
-                type: 'Web framework',
+                type: 'ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 123,
                 name: 'ASP.NET MVC',
-                description: `ASP.NET MVC`,
+                description: `ASP.NET MVC gives you a powerful, patterns-based way to build dynamic websites that enables a clean separation 
+                              of concerns and that gives you full control over markup for enjoyable, agile development`,
                 type: 'Web framework',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 124,
-                name: 'Windows mobile 6.0',
-                description: `Windows mobile 6.0`,
+                name: 'Windows Mobile',
+                description: `Windows Mobile is a family of mobile operating systems developed by Microsoft for smartphones and Pocket PCs`,
                 type: 'Framework',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 125,
                 name: 'Windows Forms',
-                description: `Windows Forms`,
+                description: `Windows Forms (WinForms) is a graphical (GUI) class library included as a part of Microsoft .NET Framework, 
+                              providing a platform to write rich client applications for desktop, laptop, and tablet PCs`,
                 type: 'Framework',
                 iconPath: './assets/img/ico/visualstudio.jpg'
             },
             {
                 id: 125,
                 name: 'Android',
-                description: `Android`,
+                description: `Android is a mobile operating system developed by Google, based on the Linux kernel and designed primarily for 
+                              touchscreen mobile devices such as smartphones and tablets`,
                 type: 'Framework',
                 iconPath: './assets/img/ico/android.png'
             },
             {
                 id: 125,
                 name: 'Java',
-                description: `Java`,
+                description: `Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented, 
+                              and specifically designed to have as few implementation dependencies as possible`,
                 type: 'Programming language',
                 iconPath: './assets/img/ico/java.png'
             },
             {
                 id: 125,
                 name: 'Ruby on Rails',
-                description: `Ruby on Rails`,
+                description: `A web-application framework that includes everything needed to create database-backed web applications 
+                              according to the Model-View-Controller (MVC)`,
                 type: 'Web framework',
                 iconPath: './assets/img/ico/rails.jpg'
             },
             {
                 id: 125,
                 name: 'Angular CLI',
-                description: `Angular CLI`,
+                description: `A command line interface for Angular`,
                 type: 'Tool',
-                iconPath: './assets/img/ico/npm.jpg'
+                iconPath: './assets/img/ico/angular.png'
             }
         ];
         const types = [
